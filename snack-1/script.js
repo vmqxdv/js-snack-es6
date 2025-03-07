16b105dc4b77f3963e0c4992d58c36b7f92a6456
vmqxdv <vquintilio@gmail.com>
2025-03-07 14:19:27 +0100

    feat: Dichiarate costanti proprietà tabella

--- snack-1/script.js
+++ snack-1/script.js
@@ -7,11 +7,17 @@ Stampare a schermo la bici con peso minore.
 */
 
 const bikesData = [
-  { nome: 'Pinarello Dogma F', peso: 6.8 },
-  { nome: 'Specialized Tarmac SL7', peso: 6.7 },
-  { nome: 'Cannondale SuperSix EVO', peso: 6.9 },
-  { nome: 'Trek Emonda SLR', peso: 6.6 },
-  { nome: 'BMC Teammachine SLR01', peso: 6.8 },
-  { nome: 'Cervélo R5', peso: 6.7 },
-  { nome: 'Wilier Zero SLR', peso: 6.5 }
+  { name: 'Pinarello Dogma F', weight: 6.8 },
+  { name: 'Specialized Tarmac SL7', weight: 6.7 },
+  { name: 'Cannondale SuperSix EVO', weight: 6.9 },
+  { name: 'Trek Emonda SLR', weight: 6.6 },
+  { name: 'BMC Teammachine SLR01', weight: 6.8 },
+  { name: 'Cervélo R5', weight: 6.7 },
+  { name: 'Wilier Zero SLR', weight: 6.5 }
 ];
+
+
+const bikesDataTable = document.getElementById('bikes-data-table');
+
+
+const bikesDataTableBody = document.createElement('tbody');
