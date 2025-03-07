13a6c7391044838814d380a4d611550ee9b0ec33
vmqxdv <vquintilio@gmail.com>
2025-03-07 15:38:19 +0100

    feat: Dichiarata funzione per ottenere la bici con li peso minore

---
+++ snack-1/script.js
@@ -0,0 +1,33 @@
+/*
+
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.
+
+*/
+
+const bikesData = [
+  { name: 'Pinarello Dogma F', weight: 6.8 },
+  { name: 'Specialized Tarmac SL7', weight: 6.7 },
+  { name: 'Cannondale SuperSix EVO', weight: 6.9 },
+  { name: 'Trek Emonda SLR', weight: 6.6 },
+  { name: 'BMC Teammachine SLR01', weight: 6.8 },
+  { name: 'Wilier Zero SLR', weight: 6.5 },
+  { name: 'Cervélo R5', weight: 6.7 },
+];
+
+
+console.log(getLowestWeight(bikesData));
+
+
+
+
+function getLowestWeight(arr) {
+  let lowestWeightBike = { name: '', weight: Infinity };
+
+  arr.forEach(bike => {
+    if (bike.weight < lowestWeightBike.weight) lowestWeightBike = bike;
+  });
+
+  return lowestWeightBike;
+};
