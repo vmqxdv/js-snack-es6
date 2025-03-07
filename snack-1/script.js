5e7e974a08eb9e4feb3b0478379dc726e0b15dbf
vmqxdv <vquintilio@gmail.com>
2025-03-07 14:03:34 +0100

    feat: Dichiarata variabile contenente nomi  peso di bici da corsa

--- snack-1/script.js
+++ snack-1/script.js
@@ -5,3 +5,13 @@ Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e pe
 Stampare a schermo la bici con peso minore.
 
 */
+
+const bikesData = [
+  { nome: 'Pinarello Dogma F', peso: 6.8 },
+  { nome: 'Specialized Tarmac SL7', peso: 6.7 },
+  { nome: 'Cannondale SuperSix EVO', peso: 6.9 },
+  { nome: 'Trek Emonda SLR', peso: 6.6 },
+  { nome: 'BMC Teammachine SLR01', peso: 6.8 },
+  { nome: 'Cervélo R5', peso: 6.7 },
+  { nome: 'Wilier Zero SLR', peso: 6.5 }
+];
